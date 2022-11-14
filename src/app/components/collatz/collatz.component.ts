e1edbc36b3bdefd51c5ac8f0fc85e6265915c7b3
juancarlosmaranon <juancar2001jcmg@gmail.com>
2022-11-14 14:27:19 +0100

    gh

---
+++ src/app/components/collatz/collatz.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collatz',
+  templateUrl: './collatz.component.html',
+  styleUrls: ['./collatz.component.css']
+})
+export class CollatzComponent implements OnInit {
+  
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
